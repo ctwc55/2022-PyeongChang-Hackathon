e3dd3058c9f1b1fa33494c89c748601662cbddc8
ctwc55 <ssy990408@gmail.com>
2022-10-22 11:56:51 +0900

    update py and htmls

--- __init__.py
+++ __init__.py
@@ -8,6 +8,10 @@ CORS(app)
 def mainPage():
     return render_template('index.html')
 
+@app.route('/<name>')
+def otherPages(name):
+    return render_template(name)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='8000')
