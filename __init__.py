8253dacc4d161a832ce094c67b62a711de283309
ctwc55 <ssy990408@gmail.com>
2022-10-23 00:02:56 +0900

    update yolov5

--- __init__.py
+++ __init__.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, Response
 from flask_cors import CORS
+#from yolov5 import fast_detect
 import cv2
+import subprocess
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)
 
 camera = cv2.VideoCapture(0)
 
-def gen_frames():  
+def gen_frames():
     while True:
         success, frame = camera.read()  # read the camera frame
         if not success:
@@ -15,8 +18,12 @@ def gen_frames():
         else:
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
+           
+            #fast_detect.run(frame)
+
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
 
 @app.route('/')
 def mainPage():
@@ -28,7 +35,6 @@ def videoPage():
 
 @app.route('/video_feed')
 def video_feed():
-    #return reder_template('webcam.html')
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/<name>')
