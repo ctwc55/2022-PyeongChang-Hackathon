e40282b4d9c2f4561583a0423a9701924a2c5031
ctwc55 <ssy990408@gmail.com>
2022-10-23 09:34:47 +0900

    update controller

--- __init__.py
+++ __init__.py
@@ -4,26 +4,60 @@ from flask_cors import CORS
 import cv2
 import subprocess
 import numpy as np
+import asyncio
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-camera = cv2.VideoCapture(0)
-
 def gen_frames():
+    #os.system("python yolov5/fast_detect")    
+    
+    while True:
+        #img = cv2.imread('static/images/img.jpg', cv2.IMREAD_COLOR)
+        camera = cv2.VideoCapture('./static/images/img.jpg')
+        success, img = camera.read()
+
+        ret, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+def gen_frames2():
+    #os.system("python yolov5/fast_detect")    
+    
+    while True:
+        #img = cv2.imread('static/images/img.jpg', cv2.IMREAD_COLOR)
+        camera = cv2.VideoCapture('./static/images/img2.jpg')
+        success, img = camera.read()
+
+        ret, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+    '''
     while True:
         success, frame = camera.read()  # read the camera frame
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
+            print(type(frame))
+            print(frame.shape)
+             
+            f = next(fast_detect.run()) 
+            
+            ret, buffer = cv2.imencode('.jpg', f)
             frame = buffer.tobytes()
-           
-            #fast_detect.run(frame)
+            
+
 
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
+    '''
 
 @app.route('/')
 def mainPage():
@@ -36,6 +70,10 @@ def videoPage():
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed2')
+def video_feed2():
+    return Response(gen_frames2(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/<name>')
 def otherPages(name):
